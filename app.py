b4295b1871576a1cb5ccca87175f432b1613cad9
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-20 23:18:09 +0530

    Add files folder to store uploaded files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
-import os.path
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = '  yomommasofatshebendslight'
@@ -18,11 +19,19 @@ def your_url():
             with open('urls.json') as urls_file:
                 urls = json.load(urls_file)
 
-            if request.form['code'] in urls.keys():
-                flash('Short name is already in use. Please try another one.')
-                return redirect(url_for('home'))
+        if request.form['code'] in urls.keys():
+            flash('Short name is already in use. Please try another one.')
+            return redirect(url_for('home'))
+        
+        if 'url' in request.form.keys():
+           urls[request.form['code']] = {'url': request.form['url']}
+        else: 
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            # print(os.cwd())
+            f.save(os.path.join(f'{os.getcwd()}/files', full_name))
+            urls[request.form['code']] = {'file': full_name}
 
-        urls[request.form['code']] = {'url': request.form['url']}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
