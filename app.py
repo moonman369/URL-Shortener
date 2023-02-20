6be25d6523f8439e8553dc413bbea831652cba23
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-20 22:23:47 +0530

    Add save url to json logic

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +10,11 @@ def home():
 @app.route('/your_url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else: return redirect(url_for('home'))
+
 
