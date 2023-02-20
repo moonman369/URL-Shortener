2772ca6e5772d23eb92849e05df6bce5c0c1d5e4
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-02-20 22:30:46 +0530

    Redirect to home if shortname is already taken

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os.path
 
 app = Flask(__name__)
 
@@ -11,6 +12,14 @@ def home():
 def your_url():
     if request.method == 'POST':
         urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+            if request.form['code'] in urls.keys():
+                return redirect(url_for('home'))
+
         urls[request.form['code']] = {'url': request.form['url']}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
